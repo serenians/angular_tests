1e1e2ba0ed8dce7c71afedc689fa77a376fba1e9
serenians <serenians@gmail.com>
2018-04-04 23:05:32 +0545

    Basic Router Set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './component/post/post.component';
+import { HomeComponent } from './component/home/home.component';
 
+var routeNode : Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+  },
+  {
+   path: 'posts',
+   component: PostComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routeNode)
   ],
   providers: [],
   bootstrap: [AppComponent]
